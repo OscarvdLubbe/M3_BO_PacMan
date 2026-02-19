d694751bfabf5765fba537e61fbc0b6cd6823233
OscarvdLubbe <40429@ma-web.nl>
2026-02-19 17:46:38 +0100

    Map for PacMan Game

---
+++ Assets/Scripts/MazeGenration.cs
@@ -0,0 +1,77 @@
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MazeGenration : MonoBehaviour
+{
+    public GameObject Wall;
+    public GameObject Path;
+    [SerializeField] GameObject Map;
+    int x;
+    int y;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // als alle orbs heeft opent passage naar 3d world
+    private int[,] mapMaker =
+    {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1,0,1,1,1,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,0,1,1,1,1,0,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,0,0,1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+    };
+
+    void Start()
+    {
+       // Wall.transform.SetParent(Map.transform,false);
+
+        Vector2 TileSize = Wall.GetComponent<Renderer>().bounds.size;
+        float ox = (mapMaker.GetLength(1) * TileSize.x - 1) / 2;
+        float oy = (mapMaker.GetLength(0) * TileSize.y - 1) / 2;
+
+        for (int y = 0; y < mapMaker.GetLength(0); y++)
+        {
+            for (int x = 0; x < mapMaker.GetLength(1); x++)
+            {
+                float ux = (x * TileSize.x) - ox;
+                float uy = (-y * TileSize.y) + oy;
+                switch (mapMaker[y, x])
+                {
+                    case 0:
+                        GameObject cube = Instantiate(Wall, new Vector3(ux, uy, -1), quaternion.identity);
+                        cube.transform.SetParent(Map.transform);
+                        //cube.GetComponent<Renderer>().material.color = Color.black;
+                        break;
+                    case 1:
+                        GameObject air = Instantiate(Path, new Vector3(ux, uy,-1), quaternion.identity);
+                        air.transform.SetParent(Map.transform);
+                        air.GetComponent<Renderer>().material.color = Color.white;
+                        break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
